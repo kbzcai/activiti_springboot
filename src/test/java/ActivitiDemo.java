710717352ec97e6a4678544db23a5408ebd5fbb4
503600776@qq.com <503600776@qq.com>
2022-09-19 17:11:05 +0800

    2022.09.19.17.10

--- src/test/java/ActivitiDemo.java
+++ src/test/java/ActivitiDemo.java
@@ -1,8 +1,10 @@
-import org.activiti.engine.ProcessEngine;
-import org.activiti.engine.ProcessEngines;
-import org.activiti.engine.RepositoryService;
+import org.activiti.engine.*;
 import org.activiti.engine.repository.Deployment;
+import org.activiti.engine.runtime.ProcessInstance;
+import org.activiti.engine.task.Task;
 import org.junit.Test;
+
+import java.util.List;
 
 public class ActivitiDemo {
 
@@ -26,9 +28,62 @@ public class ActivitiDemo {
 
 
     //启动流程实例
+    /*
+    * act_hi_actinst         流程实例执行历史
+    * act_hi_identitylink    流程的参与用户信息历史
+    * act_hi_procinst        流程实例历史信息
+    * act_hi_taskinst        流程任务历史信息
+    * act_ru_execution       流程正在执行信息
+    * act_ru_identitylink    流程的参与用户信息
+    * act_ru_task            任务信息
+    * */
     @Test
     public void testStartProcess(){
         //创建processEngine
-        //获取
+        ProcessEngine processEngine = ProcessEngines.getDefaultProcessEngine();
+        //获取RuntimeService
+        RuntimeService runtimeService = processEngine.getRuntimeService();
+        //根据流程定义的id来启动流程
+        ProcessInstance processInstance = runtimeService.startProcessInstanceByKey("myEvection");
+        //输出
+        System.out.println("流程定义id="+processInstance.getProcessDefinitionId());
+        System.out.println("流程实例id="+processInstance.getId());
+        System.out.println("当前活动的ID="+processInstance.getActivityId());
+    }
+
+    /*
+      查询个人待执行的任务
+    */
+    @Test
+    public void testFindPersonTaskList(){
+        //创建processEngine
+        ProcessEngine processEngine = ProcessEngines.getDefaultProcessEngine();
+        //获取TaskService
+        TaskService taskService = processEngine.getTaskService();
+        //根据流程的key和任务的负责人查询任务
+        List<Task> list = taskService.createTaskQuery()
+                .processDefinitionKey("myEvection")//流程key
+                .taskAssignee("zhangsan")//负责人
+                .list();
+        //输出
+        for (Task task : list) {
+            System.out.println("流程实例id="+task.getProcessInstanceId());
+            System.out.println("任务id="+task.getId());
+            System.out.println("任务负责人="+task.getAssignee());
+            System.out.println("任务名称="+task.getName());
+        }
+    }
+
+    /*
+    完成个人任务
+     */
+    @Test
+    public void complete(){
+        //获取流程引擎
+        ProcessEngine processEngine = ProcessEngines.getDefaultProcessEngine();
+        //获取taskService
+        TaskService taskService = processEngine.getTaskService();
+        //根据任务id完成任务
+        taskService.complete("2505");
     }
 }
